2668ffe2bab5c6108b61ab3c357c38b31e460b94
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-15 02:21:37 +0100

    removing warnings

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -13,7 +13,10 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href,
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
           };
         }
         return {
@@ -33,10 +36,11 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
       });
     },
