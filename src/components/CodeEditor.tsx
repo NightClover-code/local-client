9ed0b4e68c53da8c947a8542e2d3a92eaa8b703d
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-19 20:01:24 +0100

    customizing the editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,6 +2,22 @@
 import MonacoEditor from '@monaco-editor/react';
 //code editor
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
+  return (
+    <MonacoEditor
+      height="500px"
+      language="javascript"
+      theme="dark"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 export default CodeEditor;
