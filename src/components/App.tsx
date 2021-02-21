d2eb1b9611ed1da58a84f90c6d0829ab8326b196
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-21 18:11:15 +0100

    refactored App.tsx a lot more, added bundler function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,45 +1,20 @@
-//importing hooks & es build & plugins
-import { useState, useEffect, useRef } from 'react';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
-import { fetchPlugin } from '../plugins/fetch-plugin';
+//importing hooks & bundler
+import { useState } from 'react';
+import bundle from '../bundler';
 //importing components
 import CodeEditor from './CodeEditor/CodeEditor';
 import Preview from './Preview/Preview';
 //app component
 const App: React.FC = () => {
-  //refs
-  const serviceRef = useRef<any>();
   //local state
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
-  //initializing esbuild
-  const startService = async () => {
-    serviceRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
   //onClick Handler
   const onClickHandler = async () => {
-    if (serviceRef.current) {
-      //transpiling the code to es2015
-      const result = await serviceRef.current.build({
-        entryPoints: ['index.js'],
-        bundle: true,
-        write: false,
-        plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-        define: {
-          'process.env.NODE_ENV': '"production"',
-          global: 'window',
-        },
-      });
-      //updating code state with the results
-      setCode(result.outputFiles[0].text);
-    }
+    //getting output code
+    const output = await bundle(input);
+    //updating code state
+    setCode(output);
   };
   return (
     <div>
