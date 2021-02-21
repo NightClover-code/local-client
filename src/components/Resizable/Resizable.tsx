64316dde8d7e17baebfaa0e71e2a71a0cf591982
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-21 23:04:36 +0100

    improving resizable performance

--- src/components/Resizable/Resizable.tsx
+++ src/components/Resizable/Resizable.tsx
@@ -1,30 +1,52 @@
+//importing hooks
+import { useEffect, useState } from 'react';
 //importing resizable component
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 //importing styles
 import './resizable.css';
-//props interface
+//interfaces
 interface ResizableProps {
   direction: 'vertical' | 'horizental';
 }
+interface DimensionsState {
+  width: number;
+  height: number;
+}
 //resizable component
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  //local state
+  const [dimensions, setDimensions] = useState<DimensionsState>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  //getting window height and width when resizing
+  useEffect(() => {
+    const listener = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener('resize', listener);
+  }, []);
+  //resizable conditional props
   let resizableProps: ResizableBoxProps;
   if (direction === 'vertical') {
     resizableProps = {
       width: Infinity,
       resizeHandles: ['s'],
       height: 300,
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, dimensions.height * 0.9],
       minConstraints: [Infinity, 50],
     };
   } else {
     resizableProps = {
       className: 'resize-horizental',
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: dimensions.width * 0.75,
       resizeHandles: ['e'],
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [dimensions.width * 0.2, Infinity],
+      maxConstraints: [dimensions.width * 0.75, Infinity],
     };
   }
   return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
