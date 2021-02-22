258cfc0ca8e181188e73fad0e308a6ac2ac76800
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-22 18:48:48 +0100

    fixing markdown class conflicts

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,8 +1,44 @@
+//importing hooks & styles
+import { useEffect, useRef, useState } from 'react';
+import './text-editor.css';
+//react markdown editor
 import MDEditor from '@uiw/react-md-editor';
-
-const TextEditor = () => {
+//text editor component
+const TextEditor: React.FC = () => {
+  //refs
+  const divRef = useRef<HTMLDivElement | null>(null);
+  //local state
+  const [editing, setEditing] = useState(false);
+  //closing editor when clicking out
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      //checking if user clicked on the editor
+      if (
+        divRef.current &&
+        event.target &&
+        divRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      //close editor otherwise
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+    //cleanup
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+  //conditional rendering of MDEditor
+  if (editing) {
+    return (
+      <div ref={divRef}>
+        <MDEditor />
+      </div>
+    );
+  }
   return (
-    <div>
+    <div onClick={() => setEditing(true)}>
       <MDEditor.Markdown source={'# Header'} />
     </div>
   );
