21dcc03a3a737a1422824f08218295a5591190bc
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-22 23:11:38 +0100

    adding deleting cell functionnality

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,7 +1,8 @@
-//importing types
+//importing types & immer
 import { Action } from '../actions';
 import { ActionType } from '../action-types';
 import { Cell } from '../cell';
+import produce from 'immer';
 //cell state
 interface CellsState {
   data: {
@@ -19,21 +20,26 @@ const initialState: CellsState = {
   error: null,
 };
 //reducer
-const cellsReducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
-  switch (action.type) {
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    case ActionType.UPDATE_CELL:
-      return state;
-    default:
-      return state;
+const cellsReducer = produce(
+  (state: CellsState = initialState, action: Action) => {
+    switch (action.type) {
+      case ActionType.DELETE_CELL:
+        //deleting cell & removing it from order
+        delete state.data[action.payload];
+        state.order = state.order.filter(id => id !== action.payload);
+        return;
+      case ActionType.MOVE_CELL:
+        return state;
+      case ActionType.INSERT_CELL_BEFORE:
+        return state;
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        //updating state without mutating it (immer)
+        state.data[id].content = content;
+        return;
+      default:
+        return state;
+    }
   }
-};
+);
 export default cellsReducer;
