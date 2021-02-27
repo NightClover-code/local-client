13e6a5701d027bc7c38da7a9f9f805310c78b8ce
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-27 19:05:38 +0100

    adding redux action-creators

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { ActionType } from './action-types';
 import reducers from './reducers';
 //getting typescript to work with devtools
 declare global {
